c8ae7ec31a4def4c56da3bd0d3ae88b5c03c70c5
upix project <sunnylinestudio@gmail.com>
2026-01-10 12:23:02 +0300

    Render WelcomeMessage in App component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -6,6 +7,9 @@ import UserProfile from './components/UserProfile';
 function App() {
   return (
     <div>
+
+      <WelcomeMessage />
+      
       <Header />
       <MainContent />
 
