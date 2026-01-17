b1a82dc8e1201025d15d10a712f5fe5662c45a17
upix project <sunnylinestudio@gmail.com>
2026-01-18 02:00:37 +0300

    Add inline styling, counter app, context API refactor, and company website

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,17 @@
+function MainContent() {
+  return (
+    <main
+      style={{
+        padding: '20px',
+        backgroundColor: '#f4f4f4',
+        minHeight: '200px'
+      }}
+    >
+      <p style={{ fontSize: '18px' }}>
+        Welcome to my React application with inline styling!
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
