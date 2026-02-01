61949d7e9b0ac0d3f1ff68339da193d41f09e59c
upix project <sunnylinestudio@gmail.com>
2026-02-02 01:58:27 +0300

    update githubService

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -12,14 +12,14 @@ export const fetchUserData = async (username) => {
   return response.data;
 };
 
-// Task 2: advanced search (keep this)
+// Task 2: advanced search (keep this)//
 export const searchUsersAdvanced = async (username, location, minRepos, page = 1) => {
   let query = `${username}`;
   if (location) query += `+location:${location}`;
   if (minRepos) query += `+repos:>=${minRepos}`;
 
   const response = await axios.get(
-    `${SEARCH_BASE_URL}?q=${query}&page=${page}&per_page=10`,
+    `https://api.github.com/search/users?q=${query}&page=${page}&per_page=10`,
     {
       headers: TOKEN ? { Authorization: `token ${TOKEN}` } : {},
     }
@@ -27,3 +27,5 @@ export const searchUsersAdvanced = async (username, location, minRepos, page = 1
 
   return response.data;
 };
+
+
