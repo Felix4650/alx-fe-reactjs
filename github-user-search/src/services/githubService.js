5f6da3a9d8b59c40c2aee210066d4959c0812a4c
upix project <sunnylinestudio@gmail.com>
2026-02-07 17:23:00 +0300

    Render UserProfile in App component with Tailwind styling

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -15,8 +15,8 @@ export const fetchUserData = async (username) => {
 // Task 2: advanced search (keep this)//
 export const searchUsersAdvanced = async (username, location, minRepos, page = 1) => {
   let query = `${username}`;
-  if (location) query += `+location:${location}`;
-  if (minRepos) query += `+repos:>=${minRepos}`;
+  if (location) query += `+location:${location}`;  
+  if (minRepos) query += `+repos:>=${minRepos}`; 
 
   const response = await axios.get(
     `https://api.github.com/search/users?q=${query}&page=${page}&per_page=10`,
