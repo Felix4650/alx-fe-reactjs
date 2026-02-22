df205ff18348b35ae07add4af86477fd3c1cd54b
upix project <sunnylinestudio@gmail.com>
2026-02-22 20:59:24 +0300

    changed FormikForm.jsx to formikForm.js

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"; 
 import RegistrationForm from "./components/RegistrationForm";
-import FormikForm from "./components/FormikForm";
+import FormikForm from "./components/formikForm";
 
 function App() {
   const [useFormik, setUseFormik] = useState(false);
---
+++ form-handling-react/src/components/formikForm.js
@@ -0,0 +1,45 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+    username: Yup.string().required("Username is required"),
+    email: Yup.string()
+        .email("Invalid email")
+        .required("Email is required"),
+    password: Yup.string()
+        .min(6, "Password must be at least 6 characters")
+        .required("Password is required"),
+});
+
+const initialValues = {
+    username: "",
+    email: "",
+    password: "",
+};
+
+function FormikForm() {
+    return (
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={(values) => {
+                console.log(values);
+            }}
+        >
+            <Form>
+                <Field name="username" placeholder="Username" />
+                <ErrorMessage name="username" component="div" />
+
+                <Field name="email" type="email" placeholder="Email" />
+                <ErrorMessage name="email" component="div" />
+
+                <Field name="password" type="password" placeholder="Password" />
+                <ErrorMessage name="password" component="div" />
+
+                <button type="submit">Submit</button>
+            </Form>
+        </Formik>
+    );
+}
+
+export default FormikForm;
