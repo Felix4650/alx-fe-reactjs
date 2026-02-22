1dc532d73860c999d7866700d221f65bf3f1450c
upix project <sunnylinestudio@gmail.com>
2026-02-22 19:45:13 +0300

    updated form-handling-react

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState } from "react"; 
 import RegistrationForm from "./components/RegistrationForm";
-import FormikForm from "./components/formikForm";
+import FormikForm from "./components/FormikForm";
 
 function App() {
   const [useFormik, setUseFormik] = useState(false);
